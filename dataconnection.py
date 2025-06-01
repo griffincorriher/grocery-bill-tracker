4b977fd33345a3c6a60a0da327a108f3c22127f3
Griffin Corriher <57311125+griffincorriher@users.noreply.github.com>
2025-06-01 20:45:23 +0000

    Add clear data button

--- dataconnection.py
+++ dataconnection.py
@@ -1,11 +1,48 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-
+import pandas as pd
 # Read secrets
 spreadsheet = st.secrets["connections"]["gsheets"]["spreadsheet"]
 worksheet = st.secrets["connections"]["gsheets"]["worksheet"]
 
-def write_data(df):
+df = pd.DataFrame()
+
+def read_data():
+    
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    df = conn.read(
+        spreadsheet=spreadsheet,
+        worksheet=worksheet,
+        usecols=[0, 1, 2],
+    )
+
+    df['pay period'] = pd.to_datetime(df['pay period']).dt.date
+    df['grocery date'] = pd.to_datetime(df['grocery date']).dt.date
+
+
+    return df
+    # df_editor = st.data_editor(df, num_rows="dynamic")
+    
+
+def update_data(grocery_dict):
+    temp_df = read_data()
+    df = pd.concat([pd.DataFrame([grocery_dict]), temp_df], ignore_index=True)
+    
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    df = conn.update(
+        worksheet=worksheet,
+        data=df,
+    )
+    st.cache_data.clear()
+    # st.rerun()
+
+
+def refresh_data_table(grocery_dict):
+    temp_df = read_data()
+    df = pd.concat([pd.DataFrame([grocery_dict]), temp_df], ignore_index=True)
+    
     conn = st.connection("gsheets", type=GSheetsConnection)
 
     df = conn.update(
@@ -15,16 +52,16 @@ def write_data(df):
     st.cache_data.clear()
     st.rerun()
 
-def read_data():
-    
+def clear_worksheet():
     conn = st.connection("gsheets", type=GSheetsConnection)
-
-    df = conn.read(
-        spreadsheet=spreadsheet,
-        worksheet=worksheet,
-        usecols=[0, 1],
-    )
+    conn.clear(worksheet=worksheet)
+    st.info("Worksheet Example 1 Cleared!")
     st.cache_data.clear()
+
+    df = conn.update(
+        worksheet=worksheet,
+        data = pd.DataFrame({'grocery date': [],
+                            'pay period': [],
+                            'grocery bill': []}),
+    )
     st.rerun()
-    st.write(df)
-    
