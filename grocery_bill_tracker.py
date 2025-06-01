1b3329cf14c612cd5f57ceb8aeaccdb6814b6909
Griffin Corriher <57311125+griffincorriher@users.noreply.github.com>
2025-06-01 00:24:53 +0000

    Moved the main app out of the streamlit_app.py

---
+++ grocery_bill_tracker.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from datetime import datetime
+
+def grocery_bill_tracker():
+    row = st.columns(3)
+    st.title("Grocery Bill Tracker")
+    st.sidebar.metric(label="MoM Spend", value=4, delta=-0.5, delta_color="inverse")
+    st.sidebar.metric(label="YoY Spend", value=4, delta=-0.5, delta_color="inverse")
+
+    with st.form("grocery_bill_form"):
+        grocery_purchase_date = st.date_input("Grocery purchase date", datetime.now())
+        grocery_bill = st.number_input("Grocery price", value=None, placeholder="Bill amount")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write(f"Total spent this pay period:")
+    
+    return
